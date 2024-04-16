9627721a09417e43d7117ab741225fd3e3051419
shourav kumar <shouravk81@gmail.com>
2024-04-16 14:29:01 +0530

    practice

---
+++ practice all pg/abcd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int r,c;
+   for(r=1;r<=4;r++)
+   {
+    for(c=1;c<=1;c++)
+    {
+        cout<<"a b c d";
+    }
+    cout<<endl;
+   }
+    return 0;
+}
