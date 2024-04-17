6dbd19cbc7689fde3bf716c36551f5f271b118f4
shourav kumar <shouravk81@gmail.com>
2024-04-17 16:11:23 +0530

    test

---
+++ 2ndtestjay/grading.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cout<<"enter the mekse of student ";
+   cin>>n;
+   if(n>=90&&n<=100)
+   cout<<"excellent";
+   if(n>=80&&n<90)
+   cout<<"v.vgood";
+   if(n>=70&&n<80)
+   cout<<"good";
+   if(n>=60&&n<70)
+   cout<<"b";
+   if(n>=40&&n<50)
+   cout<<"c";
+    return 0;
+}
---
+++ practice all pg/sevendigit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n;
+     cout<<" enter ";
+     
+     cin>>n;
+     if(n>999999&&n<10000000)
+     
+        cout<<"yes ";
+     
+     else
+     
+        cout<<"no";
+     
+     return 0;
+             
+}
