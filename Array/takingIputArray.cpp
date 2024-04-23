d31ec080b4c5f7c7743630e4f0fe7f184525a5cc
shourav kumar <shouravk81@gmail.com>
2024-04-24 01:43:20 +0530

    work

---
+++ Array/takingIputArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number of size ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"enter the "<<i+1<<"element : ";
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<"  "<<arr[i];
+    }
+    return 0;
+}
