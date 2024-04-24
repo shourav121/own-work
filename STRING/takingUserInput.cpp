ff38d7fece2a9776da72b53bb10106a53989dae1
shourav kumar <shouravk81@gmail.com>
2024-04-24 22:01:19 +0530

    work

---
+++ STRING/takingUserInput.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    string str;
+    cin>>str;
+    cout<<str;
+return 0;
+}
+
+/********************************************************************************************************/
+
+/*COMMENT ONE CODE WHILE RUNNING FIRST CODE IS STORE ONLY ONE WORD SUCH AS:--> SAHOURAV
+/*SECOND CODE IS STORE A SENTENCE SUCH AS--> SHOURAV KUMAR*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin,str);
+    cout<<str;
+    return 0;
+}
