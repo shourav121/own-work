6e5b6bc55569b1f3ccae956cf821ed1d01a08a34
shourav kumar <shouravk81@gmail.com>
2024-05-02 23:01:39 +0530

    work

---
+++ STRING/pushback.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    string str="shour";
+    char pqr='f';
+    str.push_back(pqr);
+    cout<<str;
+    return 0;
+}
---
+++ STRING/strcat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+     char c1[10]="college";
+     char p2[10]="wallah";
+
+     strcat(c1,p2);
+     cout<<c1;
+     
+    return 0;
+}
