4f76fcb27bd0af55d2eba551a6d4ff3bac7b6924
shourav kumar <shouravk81@gmail.com>
2024-05-02 23:23:52 +0530

    work

---
+++ STRING/LowerToUpeerCase.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter a character ";
+    cin>>ch;
+    ch=ch-32;
+    cout<<"upper case  "<<ch;
+    return 0;
+
+}
