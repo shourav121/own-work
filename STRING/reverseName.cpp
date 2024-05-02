ad20f030be0745cc7646c3ff40cbb6b74bbad2cc
shourav kumar <shouravk81@gmail.com>
2024-05-02 23:57:13 +0530

    work

---
+++ STRING/reverseName.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;  
+int main()
+{
+
+    string str="shourav";
+    reverse(str.begin(),str.end());
+    cout<<str;
+    
+    return 0;
+}
