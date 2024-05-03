ba2209b7ae641a379f42bdd945684502b31d0b23
shourav kumar <shouravk81@gmail.com>
2024-05-03 23:56:25 +0530

    work

--- areaofcircle.cpp
+++ areaofcircle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   float radius,area;
-  cout<<"enter the radius of circle: "<<endl;
+  cout<<"enter the radius of an circle: "<<endl;
   cin>>radius;
   area=3.14*radius*radius;
   cout<<"the area of circle is: "<<area<<endl;
