3096314cc84cbbdb088b280aee70a304404cb41b
shourav kumar <shouravk81@gmail.com>
2024-05-05 23:33:07 +0530

    work

--- own work/factorial.cpp
+++ own work/factorial.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
     int n,fact=1;
     cout<<"enter the number ";
     cin>>n;
-    for(int i=1;i<=n;i++)
+    for(int i=1; i<=n; i++)
     {
         fact=fact*i;
     }
     cout<<fact;
-
     return 0;
 }
