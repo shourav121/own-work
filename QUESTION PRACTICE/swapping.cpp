6862834e5fda5e95513c2edd2309d06f82672591
shourav kumar <shouravk81@gmail.com>
2024-05-05 23:15:53 +0530

    swap

---
+++ QUESTION PRACTICE/swapping.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=55;
+    int b=88;
+    int c;
+    cout<<"after swap";
+    c=a;
+    a=b;
+    b=c;
+    cout<<a<<" "<<b;
+    return 0;
+}
